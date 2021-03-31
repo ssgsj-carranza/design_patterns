cf50bc38eeeaa284227443c7b446740ae88be214
unknown <ssgsj.carranza@gmail.com>
2021-03-31 10:20:10 -0500

    init

---
+++ queue.py
@@ -0,0 +1,10 @@
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        return self.queue.pop(0)
